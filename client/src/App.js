ba54d95d7890158ea87d031e45cd19147aaa9bd5
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-01-19 21:28:38 +0530

    add pages login and signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,22 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Login } from './components/Login'
+import { SignUp } from './components/SignUp'
+import { Home } from './components/Home.jsx'
 
 function App() {
-  return <div className='App'></div>
+  return (
+    <div id='app-container'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='*' element={<h1>Page not found</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App
