e0a2ae0e71b7ee3648300f088bce7cc0c7eaea8d
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-01-23 17:00:08 +0530

    refactor with pool.query()

--- server/models/queries.js
+++ server/models/queries.js
@@ -2,7 +2,7 @@ import pg from 'pg'
 import dotenv from 'dotenv'
 import path from 'path'
 
-dotenv.config({ path: path.join(process.cwd(), '.env') })
+dotenv.config({ path: path.join(process.cwd(), '.env') }) //
 
 const { Pool } = pg
 const pool = new Pool({
@@ -13,31 +13,14 @@ const pool = new Pool({
   port: process.env.DB_PORT
 })
 
-async function runQuery(query, params) {
-  const client = await pool.connect()
-  const res = await client.query(query, params)
-  client.release()
-  return res
-}
-
 export async function getRestaruants() {
-  try {
-    const query = `select * from restaurants;`
-    const params = []
-    const res = await runQuery(query, params)
-    return res.rows
-  } catch (err) {
-    return 'internal error'
-  }
+  // handle errors //
+  const res = await pool.query('select * from restaurants')
+  return res.rows
 }
 
 export async function getRestaurantMenu(restaurant_id) {
-  try {
-    const query = `select * from items where restaurant_id = $1;`
-    const params = [restaurant_id]
-    const res = await runQuery(query, params)
-    return res.rows
-  } catch (err) {
-    return 'internal error'
-  }
+  // case wrong table name, // id not exists
+  const res = await pool.query('select * from menus where restaurant_id = $1;', [restaurant_id])
+  return res.rows
 }
