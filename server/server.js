221019e481d6736f57be0ed74f595445ac027993
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-01-26 20:05:01 +0530

    use configfile instead of dotenv, add order router

--- server/server.js
+++ server/server.js
@@ -1,15 +1,16 @@
 import express from 'express'
-import dotenv from 'dotenv'
 import cors from 'cors'
 import { router as restaurantRouter } from './routes/restaurantRoute.js'
+import { router as orderRouter } from './routes/ordersRoute.js'
+import config from './config.js'
 
-dotenv.config()
-const port = process.env.PORT
+const port = config.port
 const app = express()
-app.use(cors({ origin: process.env.URL }))
+app.use(cors())
 app.use(express.json())
 
 app.use('/restaurants', restaurantRouter)
+app.use('/orders', orderRouter)
 
 app.listen(port, () => {
   console.log('server listening on port:', port)
