19b5774596edcb88c57bc32a9e76a06d78c89cbd
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-01-26 19:58:01 +0530

    restructure no changes

--- client/src/components/CartItem.jsx
+++ client/src/components/CartItem.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 function CartItem({ cartItem }) {
-  console.log('from cartItem:', cartItem)
+  //   console.log('from cartItem:', cartItem)
   return (
     <div>
-      <strong>{cartItem.name} quantity: </strong> {cartItem.count} ₹
-      {cartItem.count * cartItem.price}
+      <strong>{cartItem.name} quantity: </strong>
+      {cartItem.count} ₹{cartItem.count * cartItem.price}
     </div>
   )
 }
