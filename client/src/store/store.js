fd6540bac31d538fa5916b628bb36d76083e98e2
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-01-26 20:00:16 +0530

    correct spelling of cartReducer

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,11 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import restaurantReducer from '../slices/restaurantSlice'
-import cartRecucer from '../slices/cartSlice'
+import cartReducer from '../slices/cartSlice'
 
 const store = configureStore({
   reducer: {
     focusedRestaurant: restaurantReducer,
-    cartItems: cartRecucer
+    cartItems: cartReducer
   }
 })
 
