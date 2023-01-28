5a46007d422046d92bca35b854030dbcedc2595e
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-01-28 10:45:08 +0530

    rename response to data

--- client/src/requests.js
+++ client/src/requests.js
@@ -2,14 +2,14 @@ const baseUrl = 'http://localhost:3000'
 
 export async function getRestaurants() {
   const res = await fetch(`${baseUrl}/restaurants`)
-  const data = await res.json()
-  return data.restaurants
+  const response = await res.json()
+  return response.data
 }
 
 export async function getMenu(id) {
   const res = await fetch(`${baseUrl}/restaurants/${id}/menu`)
-  const data = await res.json()
-  return data.menu
+  const response = await res.json()
+  return response.data
 }
 
 export async function placeOrder(cart) {
