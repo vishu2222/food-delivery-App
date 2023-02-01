51257afba61cc211a1c53843eaf8a7dfb9c76aad
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-02-01 23:52:19 +0530

    move  to customers controler, add restaurantupdate

--- server/controllers/ordersController.js
+++ server/controllers/ordersController.js
@@ -1,82 +1,30 @@
-import { placeOrder, getItemPrices } from '../models/queries.js'
+import { confirmOrder, getCustomersAddress } from '../models/queries.js'
+import socket from '../sockets/sockets.js'
 
-async function validateItemsAndGetCartTotal(clientCart, restaurantId) {
-  // validate all items belong to the restaurant and return total
-  let totalPrice = 0
+export async function restaurantsOrderUpdate(req, res) {
   try {
-    const itemIds = clientCart.map((item) => item.itemId)
-    const dbCart = await getItemPrices(itemIds, restaurantId)
-
-    if (dbCart.length !== clientCart.length) {
-      return ['invalidItem', totalPrice]
+    const orderId = req.params.order_id
+    // console.log('orderId:', orderId)
+    const confirmation = req.body.confirmation
+    const io = socket.get()
+    if (confirmation === 'decline') {
+      io.emit('restaurant-update', { msg: 'declined' }) // io.to()
+      return res.json({ msg: 'order declined', status: 'success' })
     }
 
-    // use reduce
-    clientCart.forEach((clientItem) => {
-      const itemPrice = dbCart.find((dbItem) => dbItem.item_id === clientItem.itemId).price
-      const itemQuantity = clientItem.quantity
-      totalPrice += itemPrice * itemQuantity
-    })
-
-    return ['itemsValid', totalPrice]
+    await confirmOrder(orderId)
+    io.emit('restaurant-update', { msg: 'accepted' }) // io.to()
+    // io.emit() tell restaurant to start cooking
+    findDelivaryPartner(orderId)
+    res.json({ msg: 'order confirmed', status: 'success' })
   } catch (err) {
-    throw new Error('internal server error')
+    const io = socket.get()
+    io.emit('restaurant-update', { msg: 'server-err' })
+    return res.status(500).json({ err: 'internal error', msg: 'unable to confirm order' })
   }
 }
 
-export async function createOrder(req, res) {
-  try {
-    const cart = req.body
-    const orderTime = Date.now()
-    const restaurantId = cart.restaurantId
-    const orderItems = cart.items
-    const customerId = cart.customerId
-
-    if (
-      !Object.keys(cart).includes('restaurantId') ||
-      !Object.keys(cart).includes('items') ||
-      !Object.keys(cart).includes('customerId')
-    ) {
-      return res.status(400).json({ err: 'bad request', msg: 'insufficient order details' })
-    }
-
-    if (Object.keys(cart).length > 3) {
-      return res.status(400).json({ err: 'bad request', msg: 'order contains prohibited data' })
-    }
-
-    for (const item of orderItems) {
-      if (Object.keys(item).length > 2) {
-        return res.status(400).json({ err: 'bad request', msg: 'order contains prohibited data' })
-      }
-    }
-
-    if (orderItems.length === 0) {
-      return res.status(400).json({ err: 'bad request', msg: 'cannot place order, cart is empty' })
-    }
-
-    for (let i in orderItems) {
-      for (let j in orderItems) {
-        if (i !== j) {
-          if (orderItems[i].itemId === orderItems[j].itemId) {
-            return res.status(400).json({
-              err: 'bad request',
-              msg: 'cannot place order, repeated items, increment iteam quantity instead'
-            })
-          }
-        }
-      }
-    }
-
-    const [validation, total] = await validateItemsAndGetCartTotal(cart.items, restaurantId)
-    if (validation === 'invalidItem') {
-      return res
-        .status(404)
-        .json({ err: 'bad request', msg: 'cannot place order, some cart items are not found' })
-    }
-
-    await placeOrder(restaurantId, total, JSON.stringify(orderItems), orderTime, customerId)
-    return res.status(201).json({ status: 'success', msg: 'order placed' })
-  } catch (err) {
-    return res.status(500).json({ err: 'internal error', msg: 'unable to process the order' })
-  }
+async function findDelivaryPartner(orderId) {
+  const address = await getCustomersAddress(orderId)
+  console.log('address:', address)
 }
