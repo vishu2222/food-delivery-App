aed9a9c2ddda13b3ecd3a8f1e84e77d36c8906a9
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-02-01 23:55:23 +0530

    change to put request

--- server/routes/ordersRoute.js
+++ server/routes/ordersRoute.js
@@ -1,9 +1,6 @@
 import express from 'express'
-import { createOrder } from '../controllers/ordersController.js'
+import { restaurantsOrderUpdate } from '../controllers/ordersController.js'
 
 export const router = express.Router()
-router.post('/', createOrder)
 
-// router.get('/:id', getorderDetails)
-// router.put('/:id', updateOrder)
-// router.delete('/:id', deleteOrder)
+router.put('/:order_id', restaurantsOrderUpdate)
