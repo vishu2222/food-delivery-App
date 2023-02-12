5037e5cfc56031b770533574929d647e1ac46a9b
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-02-12 16:46:37 +0530

    refactor responses

--- server/controllers/restaurants.js
+++ server/controllers/restaurants.js
@@ -3,24 +3,24 @@ import { getRestaruants, getRestaruant, getMenu } from '../models/restaurants.js
 export async function getRestaurantsList(req, res) {
   try {
     const restaurants = await getRestaruants()
-    return res.json({ data: restaurants, status: 'success' })
+    return res.json(restaurants)
   } catch (err) {
-    return res.status(500).json({ err: 'internal server error', msg: 'unable to get restaurants' })
+    return res.status(500).json({ msg: 'unable to get restaurants' })
   }
 }
 
-export async function getRestaurantsMenu(req, res) {
+export async function getRestaurant(req, res) {
   try {
     const restaurantId = req.params.restaurantId
 
-    const restaturant = await getRestaruant(restaurantId)
-    if (restaturant.length === 0) {
-      return res.status(404).json({ err: 'bad request', msg: 'restaurant not found' })
+    const restaurant = await getRestaruant(restaurantId)
+    if (restaurant.length === 0) {
+      return res.status(404).json({ msg: 'restaurant not found' })
     }
 
     const menu = await getMenu(restaurantId)
-    return res.json({ data: menu, status: 'success' })
+    return res.json({ restaurant: restaurant[0], menu })
   } catch (error) {
-    res.status(500).json({ err: 'internal server error', msg: 'unable to get menu' })
+    res.status(500).json({ msg: 'unable to get restaurants menu' })
   }
 }
