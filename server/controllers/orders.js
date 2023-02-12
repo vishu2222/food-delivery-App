4ee71fb1abe42bf42f06d4f0bab087c7d9dc7a96
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-02-12 16:47:07 +0530

    refactor responses

--- server/controllers/orders.js
+++ server/controllers/orders.js
@@ -1,11 +1,12 @@
 import { getOrderId, placeOrder, getAllCustomerOrders, getAllRestaurantOrders } from '../models/orders.js'
-import { getItemPrices, getOrderStatus, fetchOrderDetails, fetchDeliveryDetails } from '../models/orders.js'
+import { getItemPrices, fetchOrderDetails } from '../models/orders.js'
 import { updateRestaurantConfirmation, updateDelivary, getItemNames } from '../models/orders.js'
 import { getRestaurantsLocation, setDelivaryPartner, updatePickup } from '../models/orders.js'
 import socket from '../sockets/sockets.js'
 
 // create order example cart =  {"restaurantId":1, "addressId":1, "items":{"1":2, "2":3}}
 export async function createOrder(req, res) {
+  // 400 validations are in middleware
   try {
     const cart = req.body
     const orderTime = Date.now()
@@ -15,11 +16,12 @@ export async function createOrder(req, res) {
     const clientCartItems = cart.items
     const itemIds = Object.keys(clientCartItems)
 
-    // need to do other validations commented in validateCart middleware
-    const dbcartItems = await getItemPrices(itemIds, restaurantId) // [ { item_id: 1, price: 100 }, { item_id: 2, price: 50 } ]
+    // need to validate restaurantId, addressId for 404
+    // dbcartItems example [ { item_id: 1, price: 100 }, { item_id: 2, price: 50 } ]
+    const dbcartItems = await getItemPrices(itemIds, restaurantId)
 
     if (dbcartItems.length !== Object.keys(clientCartItems).length) {
-      return res.status(404).json({ err: 'items not found', msg: 'some items dont belong to the restaurant' })
+      return res.status(404).json({ msg: 'some items dont belong to the restaurant' })
     }
 
     const totalAmount = dbcartItems.reduce((total, item) => {
@@ -30,46 +32,36 @@ export async function createOrder(req, res) {
     await placeOrder(restaurantId, addressId, totalAmount, JSON.stringify(clientCartItems), orderTime, customerId)
 
     const orderId = await getOrderId(customerId, restaurantId)
-    res.status(201).json({ data: orderId, msg: 'order placed, waiting for restaurant confirmation' })
+    res.status(201).json({ orderId, msg: 'order placed, waiting for restaurant confirmation' })
 
-    let notification = { type: 'new-order', restaurantId, order: { clientCartItems, orderId } }
-    notifyRestaurant(notification) // to
+    let notification = { type: 'new-order', restaurantId, order: { clientCartItems, orderId } } // better have another try catch here
+    notifyRestaurant(notification) //
     notification = { msg: 'awaiting restaurant confirmation' }
-    notifyCustomer(notification) // to
+    notifyCustomer(notification) //
   } catch (err) {
     console.log(err)
-    return res.status(500).json({ err: 'internal error', msg: 'unable to process the order' })
+    return res.status(500).json({ msg: 'unable to place the order' })
   }
 }
 
 export async function getAllOrders(req, res) {
   try {
     if (req.headers.userrole === 'customer') {
-      const customerId = 1 // need to get from middleware
-      const customerOrders = await getCustomerOrders(customerId)
-      return res.json({ data: customerOrders, status: 'success' })
+      const customerId = 1 //
+      const customerOrders = await getAllCustomerOrders(customerId)
+      return res.json(customerOrders)
     }
 
     if (req.headers.userrole === 'restaurant') {
-      const restaurantId = 1 // need to get from middleware
-      const restaurantOrders = await getRestaurantOrders(restaurantId)
-      return res.json({ data: restaurantOrders, status: 'success' })
+      const restaurantId = 1 //
+      const restaurantOrders = await getAllRestaurantOrders(restaurantId)
+      return res.json(restaurantOrders)
     }
 
-    return res.status(401).json({ err: 'unauthorized', msg: 'not permited to perform the action' })
+    return res.sendStatus(401)
   } catch (err) {
-    return res.status(500).json({ err: 'internal error', msg: 'server error' })
+    return res.status(500).json({ msg: 'unable to get orders' })
   }
-}
-
-async function getCustomerOrders(customerId) {
-  const orders = await getAllCustomerOrders(customerId)
-  return orders
-}
-
-async function getRestaurantOrders(restaurantId) {
-  const orders = await getAllRestaurantOrders(restaurantId)
-  return orders
 }
 
 export async function getOrderDetails(req, res) {
@@ -77,32 +69,22 @@ export async function getOrderDetails(req, res) {
     const customerId = 1 // get it from auth middleware
     const orderId = req.params.orderId // need to validate orderId format
 
-    let response = await getOrderStatus(orderId, customerId)
-    if (response.length === 0) {
-      return res.status(404).json({ err: 'order not found', msg: 'order details not found' })
-    }
-    const orderStatus = response[0].order_status
+    let orderDetails = await fetchOrderDetails(orderId)
 
-    if (orderStatus === 'awaiting restaurant confirmation' || orderStatus === 'searching for delivery partner') {
-      response = await fetchOrderDetails(orderId)
-      let orderDetails = response[0]
-      orderDetails = await addOrderItemNames(orderDetails)
-      return res.json({ data: orderDetails, status: 'success' })
+    if (orderDetails.length === 0) {
+      return res.status(404).json({ msg: 'order details not found' })
     }
 
-    response = await fetchDeliveryDetails(orderId)
-    let orderDetails = response[0]
-    orderDetails = await addOrderItemNames(orderDetails)
+    orderDetails = orderDetails[0]
+    orderDetails.order_items = await addOrderItemNames(orderDetails.order_items)
 
-    return res.json({ data: orderDetails, status: 'success' })
+    return res.json(orderDetails)
   } catch (error) {
-    return res.status(500).json({ err: 'internal error', msg: 'unable to get order details' })
+    return res.status(500).json({ msg: 'unable to get order details' })
   }
 }
 
-async function addOrderItemNames(orderDetails) {
-  const orderItems = orderDetails.order_items
-
+async function addOrderItemNames(orderItems) {
   let itemNames = await getItemNames(Object.keys(orderItems))
 
   itemNames = itemNames.map((item) => {
@@ -110,8 +92,7 @@ async function addOrderItemNames(orderDetails) {
     return item
   })
 
-  orderDetails.order_items = itemNames
-  return orderDetails
+  return itemNames
 }
 
 export async function updateOrder(req, res) {
@@ -127,19 +108,20 @@ export async function updateOrder(req, res) {
       return
     }
   } catch (error) {
-    res.status(500).json({ err: 'internal error', msg: 'unable to update order' })
+    res.status(500).json({ msg: 'unable to update order' })
   }
 }
 
 async function updateRestaurantsConfirmation(orderId, req, res) {
+  // update options reject or accept
   const confirmation = req.body.confirmation
   const restaurantId = 1 //
 
   if (confirmation === 'reject') {
     const orderStatus = 'cancelled'
     const rowCount = await updateRestaurantConfirmation(orderId, orderStatus)
-    if (rowCount < 1) return res.status(404).json({ err: 'order not found', msg: 'order not found' })
-    res.json({ msg: 'order cancelled', status: 'success' })
+    if (rowCount < 1) return res.status(404).json({ msg: 'order not found' })
+    res.json({ msg: 'order cancelled' })
     notifyCustomer({ msg: orderStatus })
     return
   }
@@ -147,67 +129,72 @@ async function updateRestaurantsConfirmation(orderId, req, res) {
   if (confirmation === 'accept') {
     const orderStatus = 'searching for delivery partner'
     const rowCount = await updateRestaurantConfirmation(orderId, orderStatus)
-    if (rowCount < 1) return res.status(404).json({ err: 'order not found', msg: 'order not found' })
-    res.json({ msg: 'order accepted', status: 'success' })
+    if (rowCount < 1) return res.status(404).json({ msg: 'order not found' })
+    res.json({ msg: 'order accepted' })
 
     notifyCustomer({ msg: orderStatus })
-    await assignDelivaryPartner(restaurantId, orderId)
+    assignDelivaryPartner(restaurantId, orderId)
     return
   }
 
-  return res.status(400).json({ err: 'bad request', msg: 'invalid confirmation' })
+  return res.status(400).json({ msg: 'invalid confirmation' })
 }
 
 async function assignDelivaryPartner(restaurantId, orderId) {
   try {
-    const partnerId = 1 // needs to search and find
     const restaurantLocation = await getRestaurantsLocation(restaurantId)
     if (restaurantLocation.length === 0) throw new Error('restaurant not found')
 
+    const partnerId = await findNearestDelivaryPartner(restaurantLocation)
+
     await setDelivaryPartner(orderId, partnerId)
+
     const orderStatus = 'awaiting pickup'
 
-    notifyRestaurant({ type: 'update', orderStatus }) // to
-    notifyCustomer({ msg: orderStatus }) // to
-    notifyPartner({ msg: orderStatus, location: restaurantLocation[0] }) // to
+    notifyRestaurant({ type: 'update', orderStatus }) //
+    notifyCustomer({ msg: orderStatus }) //
+    notifyPartner({ msg: orderStatus, location: restaurantLocation[0] }) //
   } catch (error) {
     const orderStatus = 'cancelled'
-    notifyRestaurant({ type: 'update', orderStatus }) //to
-    notifyCustomer({ msg: orderStatus }) // to
+    notifyRestaurant({ type: 'update', orderStatus }) //
+    notifyCustomer({ msg: orderStatus }) //s
   }
 }
 
-// const partnerId = findNearestDelivaryPartner(restaurantLocation)
-// async function findNearestDelivaryPartner(restaurantLocation) {
-// }
+async function findNearestDelivaryPartner(restaurantLocation) {
+  // need to search and find from streaming data of partners locations
+  // if not found cancell the order and update restaurant and customer
+  const partnerId = 1
+  return partnerId
+}
 
 async function updatePartnersConfirmation(orderId, req, res) {
   // update options pickup and delivary
+  // validate orderId format, check if order status is not cancelled before proceeding
   const confirmation = req.body.confirmation
   if (confirmation === 'pickup') {
     const rowCount = await updatePickup(orderId)
     if (rowCount < 1) {
-      return res.status(404).json({ err: 'order not found', msg: 'order not found' })
+      return res.status(404).json({ msg: 'order not found' })
     }
     const orderStatus = 'awaiting delivary'
-    res.json({ msg: 'pickup confirmed', status: 'success' })
+    res.json({ msg: 'pickup confirmed' })
     notifyRestaurant({ type: 'update', orderStatus }) //to
     notifyCustomer({ msg: orderStatus }) // to
     return
   }
 
   if (confirmation === 'delivered') {
-    const rowCount = await updateDelivary()
+    const rowCount = await updateDelivary(orderId)
     if (rowCount < 1) {
-      return res.status(404).json({ err: 'order not found', msg: 'order not found' })
+      return res.status(404).json({ msg: 'order not found' })
     }
     const orderStatus = 'delivered'
     res.json({ msg: 'delivery confirmed', status: 'success' })
-    notifyRestaurant({ type: 'update', orderStatus }) //to
     notifyCustomer({ msg: orderStatus }) // to
     return
   }
-  return res.status(400).json({ err: 'bad request', msg: 'invalid confirmation' })
+  return res.status(400).json({ msg: 'invalid confirmation' })
 }
 
 // notifications
