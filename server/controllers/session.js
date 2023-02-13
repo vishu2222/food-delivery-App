9f761dea0342f3a530866e6840a2de9390994844
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-02-13 19:33:00 +0530

    complete login functionality

--- server/controllers/session.js
+++ server/controllers/session.js
@@ -1,23 +1,47 @@
-import { getUserCredentials } from '../models/sessions.js'
+import { getUserDetails, createSession } from '../models/sessions.js'
 import bcrypt from 'bcrypt'
+import { v4 as uuid } from 'uuid'
 
 export async function userLogin(req, res) {
   try {
     const userName = req.body.userName
     const password = req.body.password
-    const response = await getUserCredentials(userName)
-    if (response.length === 0) return res.status(401).json({ msg: 'invalid user name' }) //redirect('http://localhost:3001/')
+    const userType = req.body.userType
 
-    const credentials = response[0]
-
-    if (await bcrypt.compare(password, credentials.password)) {
-      console.log(credentials)
-      const sessionId = uuid()
-      return
+    if (!['customer', 'restaurant', 'delivery_partner'].includes(userType)) {
+      return res.status(400).json({ msg: 'invalid user type' })
     }
 
-    res.status(401).json({ msg: 'wrong password' })
+    const userDetails = await getUserDetails(userName, userType)
+
+    if (!(await bcrypt.compare(password, userDetails.password))) {
+      return res.status(401).json({ msg: 'wrong password' })
+    }
+
+    const sessionId = uuid()
+    await createSession(sessionId, userDetails.user_id)
+
+    return res
+      .cookie('sessionId', sessionId, { maxAge: 10 * 60 * 1000, httpOnly: true })
+      .status(201)
+      .json({ userType })
   } catch (error) {
-    console.log(error)
+    if (error.message === 'invalidUserName') {
+      return res.status(401).json({ msg: 'invalid user name' })
+    }
+
+    if (error.code === '42P01') {
+      //  userType is different from actual userrole
+      return res.sendStatus(403)
+    }
+
+    console.log('userLogin', error)
+    res.sendStatus(500)
   }
 }
+
+export async function authorizeUser(req, res) {
+  return res.status(200).json({ userRole: req.userRole })
+}
+
+// const deviceid = req.headers['user-agent']
