429170f07c78644caebe5157804da611b839f46d
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-02-18 21:24:19 +0530

    remove userType and refactor

--- server/controllers/session.js
+++ server/controllers/session.js
@@ -6,13 +6,10 @@ export async function userLogin(req, res) {
   try {
     const userName = req.body.userName
     const password = req.body.password
-    const userType = req.body.userType
 
-    if (!['customer', 'restaurant', 'delivery_partner'].includes(userType)) {
-      return res.status(400).json({ msg: 'invalid user type' })
-    }
+    const userDetails = await getUserDetails(userName)
 
-    const userDetails = await getUserDetails(userName, userType)
+    // console.log('userDetails', userDetails)
 
     if (!(await bcrypt.compare(password, userDetails.password))) {
       return res.status(401).json({ msg: 'wrong password' })
@@ -22,20 +19,13 @@ export async function userLogin(req, res) {
     await createSession(sessionId, userDetails.user_id)
 
     return res
-      .cookie('sessionId', sessionId, { maxAge: 10 * 60 * 1000, httpOnly: true })
+      .cookie('sessionId', sessionId, { maxAge: 24 * 60 * 60 * 1000, httpOnly: true })
       .status(201)
-      .json({ userType })
+      .json({ user_type: userDetails.user_type })
   } catch (error) {
     if (error.message === 'invalidUserName') {
       return res.status(401).json({ msg: 'invalid user name' })
     }
-
-    if (error.code === '42P01') {
-      //  userType is different from actual userrole
-      return res.sendStatus(403)
-    }
-
-    console.log('userLogin', error)
     res.sendStatus(500)
   }
 }
