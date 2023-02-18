46500c06b8a6be20379b96dfc5640a4aef6e0157
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-02-18 21:14:45 +0530

    add requests file for delivary partner

---
+++ client/src/components/delivaryPartner/partnerRequests.js
@@ -0,0 +1,26 @@
+const baseUrl = 'http://localhost:3000'
+
+export async function getAllOrders() {
+  const res = await fetch(`${baseUrl}/orders`, {
+    credentials: 'include'
+  })
+
+  if (res.status === 401) {
+    return [res.status, null]
+  }
+
+  return [res.status, await res.json()]
+}
+
+export async function updateOrder(orderId, status) {
+  const res = await fetch(`${baseUrl}/orders/${orderId}`, {
+    headers: { 'Content-Type': 'application/json' },
+    credentials: 'include',
+    method: 'PATCH',
+    body: JSON.stringify({ status })
+  })
+  if (res.status !== 200) {
+    return [status.null]
+  }
+  return [res.status, await res.json()]
+}
