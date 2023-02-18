e147f2be0f4ecbbac1e94f15f9cd6765c5ed4500
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-02-18 21:25:49 +0530

    add customer models

---
+++ server/models/customer.js
@@ -0,0 +1,27 @@
+import pg from 'pg'
+import config from '../config.js'
+
+const { Pool } = pg
+const pool = new Pool({
+  user: config.dbUser,
+  host: config.dbHost,
+  database: config.dbName,
+  password: config.dbPwd,
+  port: config.dbPort
+})
+
+export async function addAddress(address, customerId) {
+  const params = [address.lat, address.long, address.house_no, address.locality, address.city, address.state, address.pincode, customerId]
+  console.log('params', params)
+  const res = await pool.query(
+    `insert into customer_address
+     (lat, long, house_no, locality, city, state, pincode, customer_id)
+     values ($1, $2, $3, $4, $5, $6, $7, $8)`,
+    params
+  )
+}
+
+export async function getAddress(customerId) {
+  const res = await pool.query(`select * from customer_address where customer_id=$1`, [customerId])
+  return res.rows
+}
