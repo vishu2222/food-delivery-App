34da2a703a739ca9f9ee9e57de09c48a4a3f297d
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-02-18 21:27:37 +0530

    refactor queries

--- server/models/sessions.js
+++ server/models/sessions.js
@@ -10,11 +10,13 @@ const pool = new Pool({
   port: config.dbPort
 })
 
-export async function getUserDetails(userName, userType) {
-  const query = `select * from users
-                 inner join ${userType} 
-                 on users.user_id = customer.user_id 
-                 where user_name = $1;`
+export async function getUserDetails(userName) {
+  const query = `SELECT users.user_id, user_name, user_type, password
+                 FROM users 
+                 LEFT JOIN customer ON users.user_id = customer.user_id
+                 LEFT JOIN restaurant ON restaurant.user_id = users.user_id
+                 LEFT JOIN delivery_partner ON delivery_partner.user_id = users.user_id
+                 WHERE user_name = $1;`
 
   const res = await pool.query(query, [userName])
 
@@ -33,7 +35,7 @@ export async function getSessionUserDetails(sessionId) {
   //   const query = `select * from sessions where session_id = $1`
   const query = `with cte as 
                          (
-	                      select s.session_id, s.user_id, s.created_at, u.user_name, u.user_type from sessions s
+	                        select s.session_id, s.user_id, s.created_at, u.user_name, u.user_type from sessions s
                           inner join users u on s.user_id = u.user_id 
                           where s.session_id = $1
                          )
@@ -41,17 +43,10 @@ export async function getSessionUserDetails(sessionId) {
                  from cte
                  left join customer on customer.user_id = cte.user_id
                  left join restaurant on restaurant.user_id = cte.user_id
-                 left join delivary_partner on delivary_partner.user_id = cte.user_id;`
+                 left join delivery_partner on delivery_partner.user_id = cte.user_id;`
   const params = [sessionId]
   const res = await pool.query(query, params)
 
   if (res.rowCount < 1) throw new Error('sessionDoesntExist')
   return res.rows[0]
 }
-
-// console.log(await getSessionUser('e5ac12a9-708d-4e73-9619-22a14658ed25'))
-// 'customer1'
-// select * from  users
-//  left join sessions
-// on users.user_id=sessions.user_id
-// where user_name = $1;
