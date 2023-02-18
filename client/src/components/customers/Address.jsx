7734b29a0984edda821a40aa55191f8f3aa6cc47
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-02-18 21:12:47 +0530

    take props from parent to store selected addressId

--- client/src/components/customers/Address.jsx
+++ client/src/components/customers/Address.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Address({ address, index }) {
+function Address({ address, index, setSelectedAddressId, selectedAddressId }) {
+  const [displayMsg, setDisplayMsg] = useState('')
   const city = address.city
   const hno = address.hno
   const locality = address.locality
 
+  function setAddress(address, checked) {
+    if (checked) {
+      setSelectedAddressId(address.address_id)
+      return
+    }
+    setSelectedAddressId(null)
+  }
+
   return (
     <div>
       <p>
+        <input
+          type='checkbox'
+          checked={selectedAddressId === address.address_id}
+          onChange={(e) => {
+            setAddress(address, e.target.checked)
+          }}
+        />
         {index + 1}. city: {city}. H.No: {hno}. Locality: {locality}
       </p>
+      <p>{displayMsg}</p>
     </div>
   )
 }
