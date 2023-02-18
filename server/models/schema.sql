7208f57e051e02fc29574a6535ecea7e988840ed
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-02-18 21:27:13 +0530

    make corrections to orders table

--- server/models/schema.sql
+++ server/models/schema.sql
@@ -1,26 +1,25 @@
-drop type user_type cascade;
-drop table users cascade;
-drop table customer cascade;
-drop table customer_address cascade;
-drop table delivary_partner cascade;
-drop table restaurant cascade;
-drop table food_item;
-drop type order_status_type cascade;
-drop table orders;
-drop table sessions;
+-- drop type user_type cascade;
+-- drop table users cascade;
+-- drop table customer cascade;
+-- drop table customer_address cascade;
+-- drop table delivery_partner cascade;
+-- drop table restaurant cascade;
+-- drop table food_item;
+-- drop type order_status_type cascade;
+-- drop table orders;
+-- drop table sessions;
 
 
 -- 
 CREATE TYPE user_type  AS ENUM('customer', 'restaurant', 'delivery_partner');
 --
 CREATE TABLE users(
-	user_Id			serial primary key,
+	user_id			serial primary key,
 	user_name       varchar not null unique,
 	user_type       user_type not null,
 	created_at      timestamp default NOW(),
 	password	    varchar not null
 );
-
 
 
 -- 
@@ -57,12 +56,14 @@ create table customer_address(
 	constraint fk_customer_customer_address foreign key(customer_id) references customer(customer_id)
 );
 
+-- insert into customer_address(city, customer_id) values('bangalore', 1);
 
---
-create table delivary_partner(
+
+--delivery_partner
+create table delivery_partner(
 	partner_id			        serial primary key,
 	partner_name				varchar not null,
-	availability	            boolean not null default true,
+	availability	            boolean not null default true, -- false
 	phone               		varchar not null unique,
 	user_id					    integer not null,
     constraint fk_users_partner foreign key(user_id) references users(user_id)
@@ -89,7 +90,7 @@ create table restaurant(
 create table food_item(
 	item_id				serial primary key,
 	item_name			varchar not null,
-	price 				real not null,
+	price 				integer not null,
 	description			varchar,
 	category           	varchar,
 	availability 		boolean,
@@ -99,14 +100,14 @@ create table food_item(
 );
 
 -- 
-CREATE TYPE order_status_type  AS ENUM('awaiting restaurant confirmation', 'searching for delivery partner', 'awaiting pickup', 'awaiting delivary', 'delivered', 'cancelled');
+CREATE TYPE order_status_type  AS ENUM('awaiting restaurant confirmation', 'searching for delivery partner', 'awaiting pickup', 'awaiting delivery', 'delivered', 'restaurant rejected', 'cancelled');
 
 create table orders(
 	order_id			  serial primary key,
 	order_time			  timestamp not null,
-	delivary_time		  timestamp,
+	delivery_time		  timestamp,
 	total_price			  real not null, -- use integer for price
-	order_status          order_status_type not null,
+	status                order_status_type not null,
 	customer_id			  integer not null,
 	address_id			  integer not null,
 	restaurant_id		  integer not null,
@@ -115,7 +116,7 @@ create table orders(
 	created_at			  timestamp default NOW(),
 	updated_at 			  timestamp default NOW(),
 	constraint fk_order_restaurant foreign key(restaurant_id) references restaurant(restaurant_id),
-	constraint fk_order_partner foreign key(partner_id) references delivary_partner(partner_id),
+	constraint fk_order_partner foreign key(partner_id) references delivery_partner(partner_id),
 	constraint fk_order_customer foreign key(customer_id) references customer(customer_id),
 	constraint fk_order_address foreign key(address_id) references customer_address(address_id)
 );
@@ -124,14 +125,11 @@ create table orders(
 select * from users;
 select * from customer;
 select * from restaurant;
-select * from sessions;
-select * from customer_address;
-select * from delivary_partner;
 select * from food_item;
+select * from customer_address;
 select * from orders;
-
-
-
+select * from delivery_partner;
+select * from sessions;
 
 
 
