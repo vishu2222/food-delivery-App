794b1b4f5754d91abe6e1d8258908ec7120ee571
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-02-18 21:28:45 +0530

    add auth middleware to orders routes

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { createOrder, getAllOrders, getOrderDetails, updateOrder } from '../controllers/orders.js'
 import { validateCart } from '../middleware/validateCart.js'
+import { auth } from '../middleware/auth.js'
 
 export const router = express.Router()
 
-router.post('/', validateCart, createOrder)
-router.get('/', getAllOrders)
-router.get('/:orderId', getOrderDetails)
-router.patch('/:orderId', updateOrder)
+router.post('/', auth, validateCart, createOrder)
+router.get('/', auth, getAllOrders)
+router.get('/:orderId', auth, getOrderDetails)
+router.patch('/:orderId', auth, updateOrder) // need to add auth
