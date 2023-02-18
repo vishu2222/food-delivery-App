d9e140020bee6d00ac383c9967b5aa01e24e57c4
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-02-18 21:21:44 +0530

    add address router

--- server/server.js
+++ server/server.js
@@ -3,11 +3,12 @@ import cors from 'cors'
 import config from './config.js'
 import cookieParser from 'cookie-parser'
 import { createServer } from 'http'
-import socketSetup from './sockets/sockets.js'
+import socketSetup from './models/partnerLocations.js'
 import { router as restaurantRouter } from './routes/restaurants.js'
 import { router as ordersRouter } from './routes/orders.js'
 import { router as usersRouter } from './routes/users.js'
 import { router as sessionRouter } from './routes/session.js'
+import { router as addressRouter } from './routes/customer.js'
 
 const port = config.port
 const app = express()
@@ -23,6 +24,7 @@ app.use('/restaurants', restaurantRouter)
 app.use('/orders', ordersRouter)
 app.use('/users', usersRouter)
 app.use('/sessions', sessionRouter)
+app.use('/customers', addressRouter)
 
 httpServer.listen(port, () => {
   console.log('server listening on port:', port)
