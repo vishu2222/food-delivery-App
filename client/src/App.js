76628ad7ba60aba0f20b0678d581bca5301a465e
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-02-18 21:03:25 +0530

    add browser routes for restaurant, add new address

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import Login from './components/Login.jsx'
 import Register from './components/Register'
-
 import Home from './components/Home'
+
 import CustomerHome from './components/customers/CustomerHome'
 import CustomerRegistration from './components/customers/CustomerRegistration'
 import Checkout from './components/customers/Checkout'
+import ShowRestaurant from './components/customers/Restaurant'
+import AddNewAddress from './components/customers/AddNewAddress'
 
 import RestaurantHome from './components/restaurants/RestaurantHome'
 import RestaurantRegistration from './components/restaurants/RestaurantRegistration'
-import ShowRestaurant from './components/customers/ShowRestaurant'
 
 import DelivaryPartnerHome from './components/delivaryPartner/DelivaryPartnerHome'
 import DelivaryPartnerRegistration from './components/delivaryPartner/DelivaryPartnerRegistration'
@@ -25,16 +27,17 @@ function App() {
           <Route path='/register' element={<Register />} />
 
           {/* customer request routes */}
-          <Route path='/register-customer' element={<CustomerRegistration />} />
           <Route path='/customer-Home' element={<CustomerHome />}></Route>
           <Route path='/restaurant/:restaurantId' element={<ShowRestaurant />} />
           <Route path='/check-out' element={<Checkout />}></Route>
+          <Route path='/register-customer' element={<CustomerRegistration />} />
+          <Route path='/add-address' element={<AddNewAddress />} />
 
           {/* restaurants requests routes */}
           <Route path='/register-restaurant' element={<RestaurantRegistration />} />
           <Route path='/restaurant-Home' element={<RestaurantHome />}></Route>
 
-          {/* dp requests routes */}
+          {/* partner requests routes */}
           <Route path='/register-delivary-partner' element={<DelivaryPartnerRegistration />} />
           <Route path='/delivery_partner-Home' element={<DelivaryPartnerHome />}></Route>
 
