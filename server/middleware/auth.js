957de0d8156700ea96f4c4ca11375c923850acc5
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-02-18 21:24:57 +0530

    snakeCase to cammelCase

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -11,16 +11,14 @@ export async function auth(req, res, next) {
 
     if (session.session_id !== sessionId) return res.sendStatus(401)
 
-    // console.log(session)
     req.userName = session.user_name
     req.userRole = session.user_type
     req.customerId = session.customer_id
-    req.restaurant_id = session.restaurant_id
-    req.partner_id = session.partner_id
+    req.restaurantId = session.restaurant_id
+    req.partnerId = session.partner_id
 
     next()
   } catch (error) {
-    console.log('auth:', error)
     if (error.message === 'sessionDoesntExist') {
       return res.sendStatus(401)
     }
