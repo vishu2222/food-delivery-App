508cab240d5700bc59cf1f7f57ef65aba2ec5f4e
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-02-18 21:15:18 +0530

    add item component to render menu items

---
+++ client/src/components/restaurants/Items.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Items({ item }) {
+  // console.log(item)
+  const itemName = item.item_name
+  const quantity = item.quantity
+  return (
+    <div>
+      <p>
+        {itemName}, quantity: {quantity}
+      </p>
+    </div>
+  )
+}
+
+export default Items
