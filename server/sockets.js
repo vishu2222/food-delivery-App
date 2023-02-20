b21b24b6501802e06f046ae275feb1da186d5dc6
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-02-20 13:46:14 +0530

    map socketId's to respective users

--- server/sockets.js
+++ server/sockets.js
@@ -1,5 +1,6 @@
 import { Server } from 'socket.io'
-import { socketMap } from './models/socketMap.js'
+import { restaurantMap, partnerMap, customerMap } from './models/socketMap.js'
+import { getSessionUserDetails } from './models/sessions.js'
 
 let io
 let count = 0
@@ -7,10 +8,27 @@ export default {
   init: (httpServer) => {
     io = new Server(httpServer, { cors: { origin: ['http://localhost:3001'] }, cookie: true, httpOnly: true })
 
-    io.use((socket, next) => {
-      socket.sessionId = socket.handshake.headers.cookie.split('sessionId=')[1]
-      socketMap[socket.sessionId] = socket.id
-      next()
+    io.use(async (socket, next) => {
+      try {
+        const sessionId = socket.handshake.headers.cookie.split('sessionId=')[1]
+        const session = await getSessionUserDetails(sessionId)
+
+        if (session.user_type === 'restaurant') {
+          restaurantMap[session.restaurant_id] = socket.id
+        }
+
+        if (session.user_type === 'customer') {
+          customerMap[session.customer_id] = socket.id
+        }
+
+        if (session.user_type === 'delivery_partner') {
+          partnerMap[session.partner_id] = socket.id
+        }
+
+        next()
+      } catch (error) {
+        return next(new Error('unauthorised'))
+      }
     })
 
     io.on('connection', (socket) => {
@@ -30,3 +48,10 @@ export default {
     throw new Error('socket not initialized')
   }
 }
+
+// socket.userId = session.user_id
+// socket.userRole = session.user_type
+// socket.customerId = session.customer_id
+// socket.restaurantId = session.restaurant_id
+// socket.partnerId = session.partner_id
+// socket.sessionId = sessionId
