d643e17aa6a22761a543934397d8eb0bbd266507
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-02-20 08:37:12 +0530

    add socketmap.js

---
+++ server/models/socketMap.js
@@ -0,0 +1,11 @@
+export const restaurantMap = {
+  // restaurantId: socketId
+}
+
+export const customerMap = {
+  // customerId: socketId
+}
+
+export const partnerMap = {
+  // partnerId: socketId
+}
