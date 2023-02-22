2ecddc08b057d378f0aeeb0ca22bf8648d3d046e
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-02-22 20:00:58 +0530

    removed unused code after refactor

--- client/src/components/customers/Addressess.jsx
+++ client/src/components/customers/Addressess.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import SavedAddresses from './SavedAddresses'
-import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
 function Addressess() {
-  const delivaryAddressId = useSelector((state) => state.delivaryAddressId)
   const navigate = useNavigate()
 
   return (
