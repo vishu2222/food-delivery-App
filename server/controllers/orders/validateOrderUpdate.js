40ea3dcd1da4a9ebc61c941e10922302b951a8f0
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-02-22 20:09:07 +0530

    return true or false after validation

--- server/controllers/orders/validateOrderUpdate.js
+++ server/controllers/orders/validateOrderUpdate.js
@@ -3,43 +3,43 @@ export function validateOrderUpdate(orderStatusUpdate, currentStatus, updatedBy,
   if (updatedBy === 'restaurant') {
     if (!['restaurant rejected', 'searching for delivery partner'].includes(orderStatusUpdate)) {
       res.status(400).json({ err: `invalid order status update` })
-      return 'updateNotAllowed'
+      return false
     }
 
     // if order is already accepted or rejected, restaurant is not allowed to further update the order
     if (currentStatus !== 'awaiting restaurant confirmation') {
       res.status(400).json({ err: `order already updated, current order status: ${currentStatus}` })
-      return 'updateNotAllowed'
+      return false
     }
 
-    return 'updateAllowed'
+    return true
   }
 
   // partner has only two options
   if (updatedBy === 'partner') {
     if (!['awaiting delivery', 'delivered'].includes(orderStatusUpdate)) {
       res.status(400).json({ err: 'invalid order status update' })
-      return 'updateNotAllowed'
+      return false
     }
 
     // when current status = 'awaiting pickup' allowed update is only 'awaiting delivery'
     if (currentStatus === 'awaiting pickup' && orderStatusUpdate !== 'awaiting delivery') {
       res.status(400).json({ err: 'order not yet pickedUp' })
-      return 'updateNotAllowed'
+      return false
     }
 
     // when current status = 'awaiting delivery' allowed update is only 'delivered'
     if (currentStatus === 'awaiting delivery' && orderStatusUpdate !== 'delivered') {
       res.status(400).json({ err: 'order can be only be delivered,  other updates not accepted' })
-      return 'updateNotAllowed'
+      return false
     }
 
     // not allowed to further update a delivered order
     if (currentStatus === 'delivered') {
       res.status(400).json({ err: 'order already delivered' })
-      return 'updateNotAllowed'
+      return false
     }
 
-    return 'updateAllowed'
+    return true
   }
 }
