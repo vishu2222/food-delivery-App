2f79744911b6af4bf4f6083125cc263aa6c67992
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-02-23 08:36:18 +0530

    obtain partner location from store to place marker

--- client/src/components/customers/Map.jsx
+++ client/src/components/customers/Map.jsx
@@ -1,22 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { GoogleMap, Marker, LoadScript } from '@react-google-maps/api'
-// import { useLocation } from 'react-router-dom'
 
 const containerStyle = { width: '400px', height: '400px' }
 const center = { lat: 12.972442, lng: 77.580643 }
 
 export default function Map() {
-  //   const location = useLocation()
-  //   const orderId = Number(location.pathname.split('order-details/')[1])
   const deliveryAddress = useSelector((state) => state.delivaryAddress)
+
   const customerCenter = { lat: deliveryAddress.lat, lng: deliveryAddress.long }
+  const partnerCenter = useSelector((state) => state.partnerLocation)
 
   return (
     <LoadScript googleMapsApiKey=''>
       <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={13}>
         <Marker position={center}></Marker>
         <Marker position={customerCenter}></Marker>
+        <Marker position={partnerCenter}></Marker>
       </GoogleMap>
     </LoadScript>
   )
