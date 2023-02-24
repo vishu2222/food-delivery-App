e8b9bfc248facc80a9866f5b603638be6eea3f36
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-02-24 14:32:44 +0530

    add redux persist

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,6 +1,16 @@
 import { createStore } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import reducer from './reducers.js'
 
-const store = createStore(reducer)
+const persistConfig = {
+  key: 'root',
+  storage
+}
 
-export default store
+const persistedReducer = persistReducer(persistConfig, reducer)
+
+let store = createStore(persistedReducer)
+let persistor = persistStore(store)
+const reduxStorePersister = { store, persistor }
+export default reduxStorePersister
