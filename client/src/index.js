616dd55bebc40bd0937dcca6f1d8fa528e5a1cd4
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-03-01 09:52:13 +0530

    add chakra provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,19 @@
 import React from 'react'
+import './index.css'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import reduxStorePersister from './store/index.js'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
+import { ChakraProvider } from '@chakra-ui/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <Provider store={reduxStorePersister.store}>
     <PersistGate loading={null} persistor={reduxStorePersister.persistor}>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </PersistGate>
   </Provider>
 )
