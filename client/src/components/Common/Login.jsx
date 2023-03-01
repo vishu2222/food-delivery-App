06da0dc36aa49c7a131008dbdd0b39abcdae8fdf
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-03-01 09:52:49 +0530

    style login page

--- client/src/components/Common/Login.jsx
+++ client/src/components/Common/Login.jsx
@@ -3,9 +3,12 @@ import { Link } from 'react-router-dom'
 import { useState } from 'react'
 import requests from './requests'
 import { useNavigate } from 'react-router-dom'
+import { isUserSignedIn, addUserName } from '../../store/actions'
+import { useDispatch } from 'react-redux'
 
 function Login() {
   const navigate = useNavigate()
+  const dispatch = useDispatch()
 
   const [userName, setuserName] = useState('')
   const [password, setPassword] = useState('')
@@ -18,6 +21,9 @@ function Login() {
       setErrMsg('invalid login')
       return
     }
+
+    dispatch(isUserSignedIn(true))
+    dispatch(addUserName(userName))
 
     switch (userType) {
       case 'customer':
@@ -36,19 +42,33 @@ function Login() {
   }
 
   return (
-    <div>
+    <div className=' flex justify-center items-center  bg-orange-50 pt-72 pb-96 '>
       <form onSubmit={(e) => e.preventDefault()}>
-        <h2>Login</h2>
-        <label> User Name </label>
-        <input type='text' onChange={(e) => setuserName(e.target.value)} />
-        <label> Password </label>
-        <input type='password' onChange={(e) => setPassword(e.target.value)} />
-        <button onClick={signIn}> Sign In </button>
-        <h3>
-          New User? <Link to='/register'>signup</Link>
+        <div className='mb-4' id='userName'>
+          <label className='labels'> User Name </label>
+          <input className='bg-slate-100 border-4 w-60' type='text' onChange={(e) => setuserName(e.target.value)} />
+        </div>
+
+        <div className='mb-4'>
+          <label className='labels mr-3'> Password </label>
+          <input
+            className='bg-slate-100 border-4 w-60 ml-1'
+            type='password'
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <button className='btn w-60 ml-28' onClick={signIn}>
+          Sign In
+        </button>
+        <p className=' text-red-800 ml-28'>{errMsg}</p>
+        <h3 className='mt-10'>
+          New User?
+          <Link className='btn ml-2 ' to='/register'>
+            sign up
+          </Link>
         </h3>
       </form>
-      <p>{errMsg}</p>
     </div>
   )
 }
