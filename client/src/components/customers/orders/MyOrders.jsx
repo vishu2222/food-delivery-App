5ffdc0cf9c3d07fb49575f180f1779d7faa3ade6
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-03-03 11:03:03 +0530

    commented undeveloped page

--- client/src/components/customers/orders/MyOrders.jsx
+++ client/src/components/customers/orders/MyOrders.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
-import { getAllOrders } from '../requests.js'
+// import { useState, useEffect } from 'react'
+// import { getAllOrders } from '../requests.js'
 
 function MyOrders() {
-  const [orders, setOrders] = useState([])
+  // const [orders, setOrders] = useState([])
 
-  useEffect(() => {
-    ;(async () => {
-      const res = await getAllOrders()
-      console.log(res)
-    })()
-  }, [])
-  return <div></div>
+  // useEffect(() => {
+  //   ;(async () => {
+  //     const res = await getAllOrders()
+  //     console.log(res)
+  //   })()
+  // }, [])
+  return <div>page under development</div>
 }
 
 export default MyOrders
