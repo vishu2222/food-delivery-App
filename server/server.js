8e7141d7475fcbbc60f9fc1ecab0e68c279d8b44
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-03-04 11:04:50 +0530

    serve react build from backend

--- server/server.js
+++ server/server.js
@@ -9,16 +9,27 @@ import { router as ordersRouter } from './routes/orders.js'
 import { router as usersRouter } from './routes/users.js'
 import { router as sessionRouter } from './routes/session.js'
 import { router as addressRouter } from './routes/customer.js'
+import path from 'path'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
 const port = config.port
 const app = express()
 
-app.use(cors({ origin: ['http://localhost:3001', 'http://localhost:3002'], credentials: true }))
+app.use(cors({ origin: ['http://localhost:3001', 'http://localhost:3000'], credentials: true }))
 app.use(express.json())
 app.use(cookieParser())
 
 const httpServer = createServer(app)
 socketSetup.init(httpServer)
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+  // res.sendFile(path.join(process.cwd(), 'build', 'index.html'))
+})
 
 app.use('/restaurants', restaurantRouter)
 app.use('/orders', ordersRouter)
