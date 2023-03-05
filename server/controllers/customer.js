3dfd1bd01a5cd596d043dcdb922cd051aabbd091
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-03-05 17:56:05 +0530

    add comments

--- server/controllers/customer.js
+++ server/controllers/customer.js
@@ -1,4 +1,5 @@
 import { addAddress, getAddress } from '../models/customer.js'
+// import { fetchData } from './utils'
 
 export async function addCustomerAddress(req, res) {
   try {
@@ -30,6 +31,7 @@ export async function getCustomerAddress(req, res) {
 }
 
 export async function getCustomerLocation(req, res) {
+  // console.log('entered...')
   try {
     const lat = req.body.lat
     const long = req.body.long
@@ -38,18 +40,23 @@ export async function getCustomerLocation(req, res) {
 
     if (Math.abs(lat) > 90 || Math.abs(long) > 180) return res.sendStatus(400)
 
+    // console.log('about to fetch', lat, long, 'key:', process.env.MAP_KEY)
     const response = await fetch(
       `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&key=${process.env.MAP_KEY}`
     )
 
+    // console.log('hi1')
     if (response.ok) {
       const data = await response.json()
+      console.log(data.results[0])
       return res.json({ result: data.results[0] })
     }
 
     const result = await response.json()
+    // console.log('result:', result)
     return res.status(400).json({ status: result.status })
   } catch (err) {
+    // console.log(err)
     res.sendStatus(500)
   }
 }
